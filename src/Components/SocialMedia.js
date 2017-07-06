cb7046de16003208ed1d8ef73c5175124dead940
Tavo <gasenjo@forgemedia.ca>
2017-07-06 17:28:36 -0400

    social background

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -6,7 +6,10 @@ class SocialMedia extends React.Component{
 	render(){
 		return(
 			<div className="social_main">
-				<h1>Social</h1>
+				<ul class="social_tech">
+					<li><a href="https://www.facebook.com/tavoasenjo" target="_blank" id="facebook">Facebook</a></li>
+					<li><a href="https://www.twitter.com/tavoasenjo" target="_blank"  id="twitter">Twitter</a></li>
+				</ul>
 			</div>
 			
 		)
