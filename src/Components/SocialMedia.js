17926e82bd86af61209862e805b6e062d8f0a73d
Gustavo Asenjo <Tavo@Tavos-MBP.local>
2017-07-10 07:23:46 -0400

    react-motion setup

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Social from '../Styles/Social.scss'
+import {Motion, spring} from 'react-motion';
 
 
 class SocialMedia extends React.Component{
 	render(){
 		return(
 			<div className="social_main">
+				
 				<ul className="social_tech">
+					<li>
+						<Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
+						  {value => <div className="animatest">{value.x}</div>}
+						</Motion>
+					</li>
 					<li id="facebook"><a href="https://www.facebook.com/tavoasenjo" target="_blank" id="facebook">Facebook</a></li>
 					<li id="twitter"><a href="https://www.twitter.com/tavoasenjo" target="_blank"  id="twitter">Twitter</a></li>
 				</ul>
